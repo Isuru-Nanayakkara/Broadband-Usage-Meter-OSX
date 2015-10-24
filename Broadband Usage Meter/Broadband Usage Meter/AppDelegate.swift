000518d4e81610806928ba881488ad2c17615a33
Isuru Nanayakkara <contact.isuru@gmail.com>
2015-10-24 16:47:02 +0530

    Xcode project without any changes.

---
+++ Broadband Usage Meter/Broadband Usage Meter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Broadband Usage Meter
+//
+//  Created by Isuru Nanayakkara on 10/24/15.
+//  Copyright © 2015 BitInvent. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
